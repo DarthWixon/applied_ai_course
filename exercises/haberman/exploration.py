e9c9c511778627e195d6b2361a81a1ec101ff3fd
Will Dixon <willdixon@protonmail.com>
2024-04-05 15:23:03 +0100

    add violin plots

--- exercises/haberman/exploration.py
+++ exercises/haberman/exploration.py
@@ -14,8 +14,10 @@ print(df.head())
 # rename columns in line with documentation
 df.columns = ["age", "op_year", "num_axil_nodes", "surv_status"]
 
-# remap surv_status s.t. 0 = dead, 1=alive
-df["surv_status"] = df["surv_status"].replace(to_replace={2.0: 0, 1.0: 1})
+# remap surv_status s.t. 0 = dead, 1 = alive, and make it a category
+df["surv_status"] = (
+    df["surv_status"].replace(to_replace={2.0: 0, 1.0: 1}).astype("category")
+)
 
 # plot histograms of each
 dist_plots, ((dist_ax_1, dist_ax_2), (dist_ax_3, dist_ax_4)) = plt.subplots(
@@ -33,4 +35,13 @@ plt.close()
 # plot pairplot using seaborn
 ## hue by surv_status to see differences between alive and dead
 sns.pairplot(data=df, corner=True, hue="surv_status")
+plt.show()
+
+# plot violin plots, just because they're new to me
+
+violin_fig, violin_axes = plt.subplots(nrows=3, ncols=1)
+for idx, feature in enumerate(list(df.columns)[:-1]):  # all features except the last
+    sns.violinplot(
+        x="surv_status", y=feature, data=df, ax=violin_axes[idx], hue="surv_status"
+    )
 plt.show()
