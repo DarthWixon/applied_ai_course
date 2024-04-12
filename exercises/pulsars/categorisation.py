66b8a219bc9d0c6b86d2919a967a004c5fac8a80
Will Dixon <willdixon@protonmail.com>
2024-04-12 14:53:30 +0100

    try out k means

--- exercises/pulsars/categorisation.py
+++ exercises/pulsars/categorisation.py
@@ -12,64 +12,52 @@ htru2 = fetch_ucirepo(id=372)
 
 # data (as pandas dataframes)
 X = htru2.data.features
-y = htru2.data.targets
+Y = htru2.data.targets
 
 # # variable information
 # print(htru2.variables)
 
 # sns.pairplot(data=X, corner=True, kind="kde")
 # plt.show()
-#
 
 
-def df_standardiser(df: pd.DataFrame) -> pd.DataFrame:
-    standardised_df = pd.DataFrame()
-    for col in df.columns:
-        standardised_df[col] = (df[col] - df[col].mean()) / df[col].std()
-    return standardised_df
+# try out the old k means, see what happens:
+def try_out_k_means():
+    def df_standardiser(df: pd.DataFrame) -> pd.DataFrame:
+        standardised_df = pd.DataFrame()
+        for col in df.columns:
+            standardised_df[col] = (df[col] - df[col].mean()) / df[col].std()
+        return standardised_df
+
+    # sns.pairplot(data=df_standardiser(X), corner=True, kind="kde")
+    # plt.show()
+
+    standardised_X = df_standardiser(X)
+
+    kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(standardised_X)
+    labels = kmeans.fit_predict(standardised_X)
+
+    def percentage_correct_calculator(found_labels, correct_labels):
+        num_correct = np.sum(np.logical_and(found_labels, correct_labels))
+        return (num_correct / len(found_labels)) * 100
+
+    print(
+        f"Number in category 0: {len(labels) - np.sum(labels)}. \nNumber in Category 1: {np.sum(labels)}."
+    )
+    print(
+        f"Percentage correct: {percentage_correct_calculator(found_labels=labels, correct_labels=Y.to_numpy()[:, 0]):.2f}%"
+    )
+
+    unique_labels = np.unique(labels)
+    for i in unique_labels:
+        plt.scatter(
+            standardised_X.to_numpy()[labels == i, 0],
+            standardised_X.to_numpy()[labels == i, 1],
+            label=i,
+        )
+    plt.legend()
+    plt.show()
 
 
-# sns.pairplot(data=df_standardiser(X), corner=True, kind="kde")
-# plt.show()
-
-X = df_standardiser(X)
-
-kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(X)
-print(kmeans.labels_)
-labels = kmeans.fit_predict(X)
-print(labels)
-unique_labels = np.unique(labels)
-
-# for i in unique_labels:
-#     plt.scatter(X[labels == i, 0], X[labels == i, 1], label=i)
-# plt.show()
-#
-#
-#
-#
-# c_values = [2]
-# ss_values = []
-# ss_stds = []
-# for c in c_values:
-#     ss_list = []
-#
-#     for i in range(100):
-#         fcm = FCM(n_clusters=c, m=2.0)
-#         fcm.fit(X)
-#         fcm_centers = fcm.centers
-#         fcm_labels = fcm.predict(X)
-#         fcm_soft_labels = fcm.soft_predict(X)
-#
-#         ss_scores = silhouette_samples(X, fcm_labels)
-#         ss = np.mean(ss_scores)
-#         ss_list.append(ss)
-#     ss_mean = np.mean(ss_list)
-#     ss_std = np.std(ss_list)
-#     print("Mean ss scores : {:}, for c = {:}".format(ss_mean, c))
-#     ss_values.append(ss_mean)
-#     ss_stds.append(ss_std)
-# plt.bar(c_values, ss_values, yerr=ss_stds)
-# plt.title("Silhouette scores for FCM clustering")
-# plt.xlabel("number of clusters used, c")
-# plt.ylabel("mean silhouette score")
-# plt.show()
+if __name__ == "__main__":
+    try_out_k_means()
